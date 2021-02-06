5bf152e05722dfd4de44ccef80b86fda5bb14048
Gokce Ozel <ozel.gokce.ozel@gmail.com>
2021-02-06 18:56:30 +0300

    Create Models

---
+++ UserRoles/UserRoles.Models/Models/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿namespace UserRoles.Models.Models
+{
+    public class PersonModel
+    {
+        public string Date { get; set; }
+
+        public int Age { get; set; }
+
+        private int _score;
+        public int Score
+        {
+            set => _score = value;
+            get => (int)(_score * 5) / 100;
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ UserRoles/UserRoles.Models/Models/RoleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRoles.Models.Models
+{
+    public class RoleModel
+    {
+        public int Id { get; set; }
+        public int UserID { get; set; }
+        public int RoleGroupID { get; set; }
+        public Int64 RoleID { get; set; }
+        public string GroupName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
