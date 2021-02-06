d046fa3b26ceacf33c72c73c2521ada36d56e1c2
Gokce Ozel <ozel.gokce.ozel@gmail.com>
2021-02-06 18:54:09 +0300

    Create Repository Tier

---
+++ UserRoles/UserRoles.Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace UserRoles.Repository
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> Table { get; }
+        IQueryable<T> TableNoTracking { get; }
+        T GetById(object id);
+        void Insert(T entity);
+        void Insert(IEnumerable<T> entities);
+        void Update(T entity);
+        void Update(IEnumerable<T> entities);
+        void Delete(T entity);
+        void Delete(IEnumerable<T> entities);
+        IEnumerable<T> GetSql(string sql);
+    }
+}
