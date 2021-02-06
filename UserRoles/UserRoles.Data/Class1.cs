90a5a5b00d7041044d90bbac314b7efab375f8bd
Gokce Ozel <ozel.gokce.ozel@gmail.com>
2021-02-06 18:48:14 +0300

    DB Create / use Db First

--- UserRoles/UserRoles.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace UserRoles.Data
-{
-    public class Class1
-    {
-    }
-}
--- UserRoles/UserRoles.Data/obj/Debug/netcoreapp3.1/UserRoles.Data.AssemblyInfo.cs
+++ UserRoles/UserRoles.Data/obj/Debug/netcoreapp3.1/UserRoles.Data.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("94555654-3c0a-4936-a4cb-554674e3f742")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserRoles.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
