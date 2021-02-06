51db2f0fe571b024573e08199f723a9bae510273
Gokce Ozel <ozel.gokce.ozel@gmail.com>
2021-02-06 19:11:55 +0300

    Create Role Service

---
+++ UserRoles/UserRoles.Services/Roles/IRoleService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UserRoles.Models.Models;
+
+namespace UserRoles.Services.Roles
+{
+    public interface IRoleService
+    {
+        public IServiceResponse<RoleModel> GetRoleById(int userId, int roleGroupID, Int64 roleID);
+        public IServiceResponse<RoleModel> GetRoleListByGroupId(int userId, int roleGroupID);
+    }
+}
---
+++ UserRoles/UserRoles.Services/ServiceResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace UserRoles.Services
+{
+    [Serializable]
+    public class ServiceResponse<T> : IServiceResponse<T>
+    {
+        public ServiceResponse() { }
+        public IList<T> List { get; set; }
+
+        [JsonProperty]
+        public T Entity { get; set; }
+
+        public int Count { get; set; }
+
+        public bool IsSuccessful { get; set; }
+    }
+}
